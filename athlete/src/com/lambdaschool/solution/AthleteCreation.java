0e7cfba6a49ac243f960658bceb99fa53a46d056
Vicky <vyue013@gmail.com>
2019-09-05 15:39:56 -0700

    removed unused code

--- athlete/src/com/lambdaschool/solution/AthleteCreation.java
+++ athlete/src/com/lambdaschool/solution/AthleteCreation.java
@@ -2,6 +2,5 @@ package com.lambdaschool.solution;
 
 public interface AthleteCreation
 {
-//	public void displayAthlete(String sport, String athlete);
 	public void displayAthlete();
 }
--- athlete/src/com/lambdaschool/solution/Processor.java
+++ athlete/src/com/lambdaschool/solution/Processor.java
@@ -2,7 +2,6 @@ package com.lambdaschool.solution;
 
 public interface Processor
 {
-//	void displayAthlete(String sport, String athlete);
 	void displayAthlete();
 
 }
